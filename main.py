3b36359faf9afeacac21e0465b6b24f6b65f3125
Lpopivnyak <136169348+LPopivnyak@users.noreply.github.com>
2024-08-26 16:34:09 +0300

    1

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from PyQt6.QtWidgets import *
 
+from file_kaka import write_in_file, read_from_file
+
+notes = read_from_file()
+print(notes)
+
+
+
 app = QApplication([])
 window = QWidget()
 
 text_edit = QTextEdit()
 list_notes_lbl = QLabel("список заміток")
 notes_list = QListWidget()
+notes_list.addItems(notes)
 create_note_btn = QPushButton("створити замітку")
 delate_note_btn = QPushButton("видалити замітку")
 save_note_btn = QPushButton("зберегти замітку")
@@ -40,8 +48,25 @@ v1.addWidget(searche_note_btn)
 main_line.addLayout(v1)
 
 
+def show_note():
+    key = notes_list.selectedItems()[0].text()
+    text_edit.setText(notes[key]['текст'])
+    tags_list.clear()
+    tags_list.addItems(notes[key]['теги'])
 
 
+
+def save_note():
+    notes[key] = field_text.toPlainText()
+    write_in_file(notes)
+
+
+
+
+
+
+notes_list.itemClicked.connect(show_note)
+save_note_btn.clicked.connect(save_note)
 window.setLayout(main_line)
 window.show()
 app.exec()
